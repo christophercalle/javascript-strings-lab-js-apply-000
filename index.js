08bb5831d51d97cfbfc5ca7eca7f7bb648efa9c3
Christopher Calle <christophercalle@gmail.com>
2018-06-02 04:36:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var greeting = "Hello, everybody!";
+var greeting = "Hello";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = `Hello, ${specialGuest}!`;
+=======
+var SpecialGuest = "Hello" + specialGuest + "!";
+>>>>>>> a513a0c24921af20b7e3c2e4bed992958a19aab7
 
-var topic = "space";
+var topic = " ";
 
+<<<<<<< HEAD
 var conversation = `Let's talk about ${topic}.`;
+=======
+var conversation = `${topic}`;
+>>>>>>> a513a0c24921af20b7e3c2e4bed992958a19aab7
 
 
